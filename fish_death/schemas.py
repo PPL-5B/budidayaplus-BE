2214a57afbd92dcac1d9712b3c58e24be6c72fb5
codaaa19 <tsabitiphone@gmail.com>
2025-03-20 17:38:45 +0700

    SCRUM-116 [GREEN]: Adding API Code and changing schema for API purposes

--- fish_death/schemas.py
+++ fish_death/schemas.py
@@ -3,11 +3,14 @@ from datetime import datetime
 from pydantic import UUID4
 from typing import List
 from user_profile.schemas import UserSchema
+from typing import Optional
 
 class FishDeathCreateSchema(Schema):
     fish_death_count: int
-    fish_alive_count: int
-    recorded_at: datetime
+    # fish_alive_count: int
+    # recorded_at: datetime
+    fish_alive_count: Optional[int] = None
+    recorded_at: Optional[datetime] = None
 
 class FishDeathOutputSchema(Schema):
     id: UUID4
