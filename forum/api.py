87b723c7f53203b3bbcc47e6c40a9d39087ae0d1
novrizair <novrizal.airsyahputra@ui.ac.id>
2025-03-26 21:06:55 +0700

    SCRUM-92 [GREEN] Implement Create Forum API and ensure that it works

--- forum/api.py
+++ forum/api.py
@@ -1,22 +1,43 @@
 from ninja import Router
+from ninja.responses import Response
 from uuid import UUID
-from django.http import Http404
 from django.contrib.auth.models import User
-from forum.schemas import ForumUpdateSchema, ForumOutputSchema
+from forum.schemas import ForumUpdateSchema, ForumOutputSchema, ForumCreateSchema
 from forum.repositories.forum_repository import ForumRepository
-from django.shortcuts import get_object_or_404
+from ninja_jwt.authentication import JWTAuth
+
+
+
 
 router = Router()
 
-@router.put("/{forum_id}", response=ForumOutputSchema)
-def update_forum(request, forum_id: UUID, data: ForumUpdateSchema):
+
+@router.post("/create", response=ForumOutputSchema, auth=JWTAuth())
+def create_forum(request, data: ForumCreateSchema):
     """
-    Endpoint untuk memperbarui deskripsi forum.
+    Endpoint untuk membuat forum baru.
+    Pengguna harus login untuk bisa membuat postingan forum.
+    Jika `parent_id` diberikan, maka postingan ini akan menjadi reply ke forum lain.
     """
-    forum = ForumRepository.get_forum_by_id(forum_id)
-    
-    if request.user != forum.user:
-        return {"error": "You are not authorized to update this forum post."}, 403
-    
-    updated_forum = ForumRepository.update_forum(forum_id, data.description)
-    return updated_forum
+    # print("Headers:", request.headers)
+    # print("Request user:", request.user)
+    # print("Is authenticated:", request.user.is_authenticated)
+    # print("User details:", vars(request.user) if request.user else "No user")
+    # Check authentication
+    if not request.user.is_authenticated: return Response({"error": "You are not authorized to create this forum post."}, status=403)
+   
+    parent_forum = None
+    if data.parent_id:
+        try:
+            parent_forum = ForumRepository.get_forum_by_id(UUID(data.parent_id))
+        except Exception:
+            return Response({
+                "error": "Invalid parent forum ID"
+            }, status=400)
+   
+    new_forum = ForumRepository.create_forum(
+        user=request.user,
+        description=data.description,
+        parent=parent_forum
+    )
+    return new_forum
