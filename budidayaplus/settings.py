84f859d40e512994b0065835adcdd30219cf07f1
rzapriono <apriono.reza@gmail.com>
2025-04-09 17:37:38 +0700

    adding fe url in allowed hosts

--- budidayaplus/settings.py
+++ budidayaplus/settings.py
@@ -36,7 +36,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     "https://dkn-budidayaplus.vercel.app",
     "https://dkn-budidayaplus-staging.vercel.app",
-    "https://budidayaplus-fe-gray.vercel.app"
+    "https://budidayaplus-fe-gray.vercel.app",
+    "https://budidayaplus-fe-alpha.vercel.app/"
 ]
 
 
@@ -141,7 +142,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://dkn-budidayaplus.vercel.app",
     "https://dkn-budidayaplus-staging.vercel.app",
     "http://localhost:8080",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://budidayaplus-fe-alpha.vercel.app/"
 ]
 
 CORS_ALLOW_METHODS = [
