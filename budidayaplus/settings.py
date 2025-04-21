b0eda1a8256d98bde31eb484415c82f1bb5e3fd4
codaaa19 <tsabitiphone@gmail.com>
2025-04-21 23:51:19 +0700

    [REFACTOR]: Changing db for deployment purposes

--- budidayaplus/settings.py
+++ budidayaplus/settings.py
@@ -1,4 +1,3 @@
-
 """
 Django settings for budidayaplus project.
 
@@ -198,8 +197,12 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
