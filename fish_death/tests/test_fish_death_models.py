cb7ec1ea0ffc6ef9879e4ee092bd534669b36e96
vinamyrnauli <vinasllgn@gmail.com>
2025-05-16 01:07:24 +0700

    test(fish_death): clean up model test with setUpTestData and docstring

--- fish_death/tests/test_fish_death_models.py
+++ fish_death/tests/test_fish_death_models.py
@@ -6,31 +6,34 @@ from cycle.models import Cycle
 from fish_death.models import FishDeath
 
 class FishDeathModelTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='081234567890', password='password')
-        self.pond = Pond.objects.create(
-            owner = self.user,
-            name = 'Test Pond',
-            image_name = 'test.jpg',
-            length = 1.0,
-            width = 1.0,
-            depth = 1.0
+    @classmethod
+    def setUpTestData(cls):
+        cls.now = datetime.now()
+        cls.user = User.objects.create_user(username='081234567890', password='password')
+        cls.pond = Pond.objects.create(
+            owner=cls.user,
+            name='Test Pond',
+            image_name='test.jpg',
+            length=1.0,
+            width=1.0,
+            depth=1.0
         )
         starting_date = datetime.strptime('2024-09-01', '%Y-%m-%d')
         ending_date = starting_date + timedelta(days=60)
-        self.cycle = Cycle.objects.create(
-            supervisor=self.user,
+        cls.cycle = Cycle.objects.create(
+            supervisor=cls.user,
             start_date=starting_date,
             end_date=ending_date
         )
-        self.fish_death = FishDeath.objects.create(
-            pond = self.pond,
-            reporter = self.user,
-            cycle = self.cycle,
-            recorded_at = datetime.now(),
-            fish_death_count = 10,
-            fish_alive_count = 90,
+        cls.fish_death = FishDeath.objects.create(
+            pond=cls.pond,
+            reporter=cls.user,
+            cycle=cls.cycle,
+            recorded_at=cls.now,
+            fish_death_count=10,
+            fish_alive_count=90,
         )
 
-    def test_str_method(self):
+    def test_str_method_returns_id(self):
+        """Should return ID as string representation of FishDeath"""
         self.assertEqual(str(self.fish_death), str(self.fish_death.id))
