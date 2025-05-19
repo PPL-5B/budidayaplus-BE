b97aa59bd5b6585b5ab06b1e9c484eb33fc26a19
codaaa19 <tsabitiphone@gmail.com>
2025-05-19 07:41:58 +0700

    Changing load sh

--- scripts/load_fixtures.sh
+++ scripts/load_fixtures.sh
@@ -6,14 +6,13 @@ set -x  # tampilkan semua perintah saat dieksekusi
 docker-compose exec -T web python manage.py flush --noinput
 
 # Load fixtures satu per satu
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/01_user.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/02_profile.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/03_pond.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/04_cycle.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/05_pondfishamount.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/06_pondquality.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/07_fishdeath.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/08_fishsampling.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/09_foodsampling.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata seeding/fixtures/10_forum.json --verbosity 2
-docker-compose exec -T web python manage.py loaddata tasks/fixtures/data.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/01_user.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/03_pond.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/04_cycle.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/05_pondfishamount.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/06_pondquality.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/07_fishdeath.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/08_fishsampling.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/09_foodsampling.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/seeding/fixtures/10_forum.json --verbosity 2
+docker-compose exec -T web python manage.py loaddata /app/tasks/fixtures/data.json --verbosity 2
